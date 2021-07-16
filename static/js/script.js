9a0be8894b154b938ae39f39262e55cfd58b1d66
TJones305 <tim.w.j@hotmail.com>
2021-07-16 16:29:00 +0000

    Added funtionality to create posts and assigned link to base and profile

---
+++ static/js/script.js
@@ -0,0 +1,9 @@
+ //Dropdown on form
+ $(document).ready(function(){
+    $('select').formSelect();
+  });
+
+  //Picker on form
+   $(document).ready(function(){
+    $('.datepicker').datepicker();
+  });
