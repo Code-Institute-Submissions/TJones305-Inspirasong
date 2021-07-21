cf13afb733693af0c0634566f9e584610731208a
TJones305 <tim.w.j@hotmail.com>
2021-07-21 20:54:21 +0000

    accidental delete

---
+++ static_copy/js/script.js
@@ -0,0 +1,19 @@
+ //SideNav
+   $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+        
+ //Dropdown on form
+ $(document).ready(function(){
+    $('select').formSelect();
+  });
+
+  //Picker on form
+   $(document).ready(function(){
+    $('.datepicker').datepicker();
+  });
+
+  //Collapsible on genre posts
+   $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
