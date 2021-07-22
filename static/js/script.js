5538510b805508b10f389c6f33009989544e0282
TJones305 <tim.w.j@hotmail.com>
2021-07-22 17:29:51 +0000

    corrected typing errors

--- static/js/script.js
+++ static/js/script.js
@@ -1,24 +1,9 @@
- //SideNav
+ //Materialize Components
    $(document).ready(function(){
     $('.sidenav').sidenav();
+    $('select').formSelect();
+    $('.datepicker').datepicker();
+    $('.collapsible').collapsible();
+      $('.modal').modal();
   });
         
- //Dropdown on form
- $(document).ready(function(){
-    $('select').formSelect();
-  });
-
-  //Picker on form
-   $(document).ready(function(){
-    $('.datepicker').datepicker();
-  });
-
-  //Collapsible on genre posts
-   $(document).ready(function(){
-    $('.collapsible').collapsible();
-  });
-
-  //Modal for delete
-   $(document).ready(function(){
-    $('.modal').modal();
-  });
